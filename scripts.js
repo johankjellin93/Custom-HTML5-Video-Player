9f855e2fe70cd0d5c1610b4c5406f894ab1de6b6
Unknown <johankjellin93@gmail.com>
2018-03-20 23:44:42 +0100

    complete

--- scripts.js
+++ scripts.js
@@ -55,3 +55,4 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+ 
