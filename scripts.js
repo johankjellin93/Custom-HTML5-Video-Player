9bc26c5862ae71e78994424e73ae56cc102a9668
Unknown <johankjellin93@gmail.com>
2018-03-20 23:33:41 +0100

    added skip function and event listener

--- scripts.js
+++ scripts.js
@@ -19,8 +19,15 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
 //Event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
