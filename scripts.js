f811f1765e0fe7508dbc122db9fc9b0736ffedbb
Unknown <johankjellin93@gmail.com>
2018-03-20 23:44:25 +0100

    added scrub function and event listeners for it

--- scripts.js
+++ scripts.js
@@ -33,6 +33,11 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
 //Event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -46,3 +51,7 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
